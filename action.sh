833f8412bf339ba077b4f49c6948947a8717bbe9
Senkusa <82468100+Senkusa@users.noreply.github.com>
2021-12-25 22:33:26 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -4,3 +4,5 @@ cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
+
+pip install speedtest-cli
