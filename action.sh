d5d2136ae384e6c6e13022f2821b9dccc11b73d3
cir.no <cirno@cognant.tech>
2022-01-01 15:31:10 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,5 @@
-git clone -b dev https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
+git clone https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp .env /root/TeamUltroid/.env
 cd /root/TeamUltroid
-pip3 install cfscrape
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
